872081ec2d78b411caae2d17bf6f26f8520dab02
Ki Suh Lee <kslee@cs.cornell.edu>
2015-11-09 10:53:55 -0500

    demo3 fix

--- demo/demo3.sh
+++ demo/demo3.sh
@@ -28,7 +28,7 @@ COVERT_FILE=$SONIC_PATH/bin/sonic.py        # FIXME
 COVERT_GENERATED=$SONIC_PATH/bin/tmp.info
 
 ssh -i ./.ssh/id_rsa2 $SONIC_SERVER "$SONIC_PATH/bin/covert.py -i $COVERT_FILE -o $COVERT_GENERATED -e $epsilon -g $idle -l $pkt_len -n $pkt_cnt"
-ssh -i ~/.ssh/id_rsa2 $SONIC_SERVER "$SONIC_PATH/bin/run_sonic.sh -m pkt_gen,pkt_cap -d 35 -w 32 --mac_src $mac_src --ip_src $ip_src --port_src $port_src --mac_dst $mac_dst --ip_dst $ip_dst --port_dst $port_dst -c $pkt_cnt -l $pkt_len -i $idle -e $epsilon -g 3 -p $SONIC_PATH/result/${prefix}_covert_${epsilon} -r $SONIC_PATH/bin/tmp.info"
+ssh -i ~/.ssh/id_rsa2 $SONIC_SERVER "$SONIC_PATH/bin/run_sonic.sh -m pkt_rpt,pkt_cap -d 35 -w 32 --mac_src $mac_src --ip_src $ip_src --port_src $port_src --mac_dst $mac_dst --ip_dst $ip_dst --port_dst $port_dst -c $pkt_cnt -l $pkt_len -i $idle -e $epsilon -g 3 -p $SONIC_PATH/result/${prefix}_covert_${epsilon} -r $SONIC_PATH/bin/tmp.info"
 ssh -i ~/.ssh/id_rsa2 $SONIC_SERVER "$SONIC_PATH/bin/ber_separate2.py -i $SONIC_PATH/result/${prefix}_covert_${epsilon}_${pkt_len}_${idle}.tmp -d $idle -m $COVERT_FILE" 
 scp -i ~/.ssh/id_rsa2 $SONIC_SERVER:$SONIC_PATH/result/${prefix}_covert_${epsilon}_${pkt_len}_${idle}* result/.
 gnuplot -e "input_file1=\"result/${prefix}_covert_${epsilon}_${pkt_len}_${idle}.tmp.one.ipd\"; input_file2=\"result/${prefix}_covert_${epsilon}_${pkt_len}_${idle}.tmp.zero.ipd\"; output_file=\"result/${prefix}_covert_${epsilon}_${pkt_len}_${idle}_pdf.eps\";" covert1.gnuplot
